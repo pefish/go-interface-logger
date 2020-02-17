dacb5cfb734a65fa7dc3e7190ed3f8e6d5adad4d
joy <joy.lai@zg.com>
2020-02-17 15:38:50 +0800

    v0.0.1

---
+++ interface_logger.go
@@ -0,0 +1,15 @@
+package go_interface_logger
+
+type InterfaceLogger interface {
+	Close()
+
+	Debug(args ...interface{})
+	DebugF(format string, args ...interface{})
+	Info(args ...interface{})
+	InfoF(format string, args ...interface{})
+	Warn(args ...interface{})
+	WarnF(format string, args ...interface{})
+	Error(args ...interface{})
+	ErrorF(format string, args ...interface{})
+}
+
